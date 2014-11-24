5c472909df87668323f1a05841d4a3a02b70080f
Martin GUIBERT <martin@keecker.com>
2014-11-24 16:05:30 +0100

    add feed to inbox could not use bulk add due to payload size...

--- app.py
+++ app.py
@@ -2,6 +2,34 @@
 
 import yaml
 import feedparser
+import config
+import pocket
+from pocket import Pocket
+import dbm
+
+# Open database, creating it if necessary.
+db = dbm.open('cache', 'c')
+if not "access_token" in db :
+	request_token = Pocket.get_request_token(consumer_key=config.consumer_key, redirect_uri=config.redirect_uri)
+	db["request_token"] = request_token
+	print 'request_token ' + db["request_token"]
+
+	# URL to redirect user to, to authorize your app
+	auth_url = Pocket.get_auth_url(code=db["request_token"], redirect_uri=config.redirect_uri)
+	print 'Go to ' + auth_url + " to authorize the script on your pocket account"
+	i = raw_input("Press Enter to continue...")
+
+	try:
+		user_credentials = Pocket.get_credentials(consumer_key=config.consumer_key, code=db["request_token"])
+	except pocket.RateLimitException, e:
+		print "Unable to auth..." + str(e)
+		db.close()
+		exit(1)
+	db["access_token"] = user_credentials["access_token"]
+
+print 'access_token ' + db["access_token"]
+pocket_instance = pocket.Pocket(config.consumer_key, db["access_token"])
+
 
 stream = open('feeds.yaml', 'r')
 feeds = yaml.load(stream)
@@ -12,15 +40,6 @@ for feed in feeds['feeds']:
     for entry in content['entries']:
     	print entry['title'] 
     	print entry['link']
-    	date = None;
-    	try:
-    		date = entry.updated_parsed
-    	except AttributeError:
-    		try:
-    			date = entry.published_parsed
-    		except AttributeError:
-    			try:
-    				date = entry.created_parsed
-    			except AttributeError:
-    				date = None
-    	print date
+    	pocket_instance.bulk_add(url=entry['link'], title=entry['title'], tags=','.join(feed["tags"]),wait=False)
+
+db.close();
